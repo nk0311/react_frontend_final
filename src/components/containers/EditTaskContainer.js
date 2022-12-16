2b42f74aeac85a2313bf04c0e3364fc076191ef3
Navait Kumar <navaitk031@gmail.com>
2022-12-15 21:23:56 -0500

    added dropdown implementation to editTaskContainer

--- src/components/containers/EditTaskContainer.js
+++ src/components/containers/EditTaskContainer.js
@@ -1,119 +1,8 @@
-// import { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { Redirect } from 'react-router-dom';
-
-// import { fetchTaskThunk, editTaskThunk } from '../../store/thunks';
-
-
-// class EditTaskContainer extends Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//           description: "", 
-//           prioritylevel: "",
-//           completionstatus: "",
-//           employeeId: null, 
-//           redirect: false, 
-//           redirectId: null
-//         };
-//     }
-
-//     componentDidMount() {
-//         //getting task ID from url
-//         this.props.fetchTask(this.props.match.params.id);
-//         this.setState({
-//             description: this.props.task.description, 
-//             prioritylevel: this.props.task.prioritylevel,
-//             completionstatus: this.props.task.completionstatus,
-//             employeeId: this.props.task.employeeId, 
-//         });
-//       }
-
-//     handleChange = event => {
-//       this.setState({
-//         [event.target.name]: event.target.value
-//       });
-//     }
-
-//     handleSubmit = event => {
-//         event.preventDefault();
-//         //get new info for task from form input
-//         let task = {
-//             id: this.props.task.id,
-//             description: this.state.description,
-//             prioritylevel: this.state.prioritylevel,
-//             completionstatus: this.state.completionstatus,
-//             employeeId: this.state.employeeId
-//         };
-        
-//         this.props.editTask(task);
-
-//         this.setState({
-//           redirect: true, 
-//           redirectId: this.props.task.id
-//         });
-
-//     }
-
-//     componentWillUnmount() {
-//         this.setState({redirect: false, redirectId: null});
-//     }
-
-//     render() {
-//       //go to single task view of the edited task
-//         if(this.state.redirect) {
-//           return (<Redirect to={`/task/${this.state.redirectId}`}/>)
-//         }
-
-//         return (
-//             <form style={{textAlign: 'center'}} onSubmit={(e) => this.handleSubmit(e)}>
-//             <label style= {{color:'#11153e', fontWeight: 'bold'}}>Description: </label>
-//             <input type="text" name="description" value={this.state.description} onChange ={(e) => this.handleChange(e)}/>
-//             <br/>
-
-//             <label style={{color:'#11153e', fontWeight: 'bold'}}>Priority Level: </label>
-//             <input type="text" name="prioritylevel" value={this.state.prioritylevel} onChange={(e) => this.handleChange(e)}/>
-//             <br/>
-
-//             <label style={{color:'#11153e', fontWeight: 'bold'}}>Completion Status: </label>
-//             <input type="text" name="completionstatus" value={this.state.employeeId} onChange={(e) => this.handleChange(e)} />
-//             <br/>
-  
-//             <label style={{color:'#11153e', fontWeight: 'bold'}}>EmployeeId: </label>
-//             <input type="text" name="employeeId" value={this.state.employeeId} onChange={(e) => this.handleChange(e)} />
-//             <br/>
-  
-//             <button type="submit">
-//               Submit
-//             </button>
-
-//           </form>
-//         )
-//     }
-// }
-
-// // map state to props
-// const mapState = (state) => {
-//     return {
-//       task: state.task,
-//     };
-//   };
-
-// const mapDispatch = (dispatch) => {
-//     return({
-//         editTask: (task) => dispatch(editTaskThunk(task)),
-//         fetchTask: (id) => dispatch(fetchTaskThunk(id)),
-
-//     })
-// }
-
-// export default connect(mapState, mapDispatch)(EditTaskContainer);
 import { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
 
 import { fetchTaskThunk, editTaskThunk, fetchAllEmployeesThunk  } from '../../store/thunks';
-
 
 /*
 IMPORTANT: comments regarding implementation details!!
@@ -178,7 +67,20 @@ class EditTaskContainer extends Component {
       });
     }
 
-   
+    handleSelectChange = event => {
+      //handle change for the dropdown menu
+      //want to set the employeeId based on the selected choice
+      //when the form gets submitted, this is how we can change
+      //assigned employee without having to manually enter in the 
+      //employeeId like before
+      if (event.target.value === "staff") {
+        this.setState({employeeId:null});
+      } else {
+        this.setState({employeeId: event.target.value})
+      }
+    }
+
+    
 
     handleSubmit = event => {
         event.preventDefault();
