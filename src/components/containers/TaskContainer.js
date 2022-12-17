22ab7825ad7ab3e592f303aff5ac57ace14d1a15
Kevin635 <experthead07@gmail.com>
2022-12-17 18:55:23 -0500

    Got the edit employee feature working

--- src/components/containers/TaskContainer.js
+++ src/components/containers/TaskContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchTaskThunk } from "../../store/thunks";
+import { fetchTaskThunk, editEmployeeThunk } from "../../store/thunks";
 import { TaskView } from "../views";
 
 class TaskContainer extends Component {
@@ -13,6 +13,7 @@ class TaskContainer extends Component {
     return (
       <TaskView 
         task={this.props.task}
+        editEmployee={this.props.editEmployee}
       />
     );
   }
@@ -29,6 +30,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchTask: (id) => dispatch(fetchTaskThunk(id)),
+    editEmployee: (employee) => dispatch(editEmployeeThunk(employee)),
   };
 };
 
